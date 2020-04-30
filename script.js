55b1dd395e09f302283dfabf4b84b74567fe66ce
Jason Scanzoni <jason@scnz.co>
2020-04-29 23:29:20 -0400

    update some links

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ function switchPilot(persona){
         SRE: {
             name: 'Bryce',
             role: 'Site Reliability Engineer',
-            image: 'Character_PNGs/Brice/Brice-1.png'
+            image: './images/1.png'
         },
         MIM: {
             name: 'Heather',
             role: 'Major Incident Manager',
-            image: 'Character_PNGs/Heather-UX-Ui/Heather-UX-Ui_1.png'
+            image: './images/2.png'
         },
         ENG: {
             name: 'Carl',
             role: 'Engineer',
-            image: 'Character_PNGs/Carl/Carl-2.png'
+            image: './images/3.png'
         },
         DEV: {
             name: 'Lana',
             role: 'Developer',
-            image: 'Character_PNGs/End-User-Woman/Woman-end-user_7.png'
+            image: './images/4.png'
         },
         EXC: {
             name: 'Sanjay',
             role: 'Executive',
-            image: 'Character_PNGs/Sanjay/Sanjay-2.png'
+            image: './images/5.png'
         },
         USR: {
             name: 'Shawn',
             role: 'User',
-            image: 'Character_PNGs/End-User-Man/Man-end-user_3.png'
+            image: './images/6.png'
         },
 
     }
